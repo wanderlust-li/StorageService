82f05c22ebb4d2072b6cda27f9a8e62537f61734
wanderlust-li <zhekalichm4n@icloud.com>
2023-09-07 15:11:25 +0300

    add get method in controller

--- StorageService.API/Controllers/AdvertisementController.cs
+++ StorageService.API/Controllers/AdvertisementController.cs
@@ -1,6 +1,9 @@
-﻿using AutoMapper;
+﻿using System.Net;
+using AutoMapper;
+using Infrastructure.Application.DTO;
 using Infrastructure.Application.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
+using StorageService.Domain.Entities;
 
 namespace StorageService.API.Controllers;
 
@@ -12,6 +15,7 @@ public class AdvertisementController : Controller
     private readonly IAdvertisementRepository _db;
     private readonly ILogger<AdvertisementController> _logger;
     private readonly IMapper _mapper;
+    private readonly APIResponse _response;
 
     public AdvertisementController(IAdvertisementRepository db, ILogger<AdvertisementController> logger, 
         IMapper mapper)
@@ -19,12 +23,42 @@ public class AdvertisementController : Controller
         _db = db;
         _logger = logger;
         _mapper = mapper;
+        this._response = new();
     }
 
-    [HttpGet("{id:int}")]
-    public async Task GetAdvertisement(int id, [FromQuery] string fields = "")
+    [HttpGet("{id:int}", Name = "GetAdvertisement")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<APIResponse>> GetAdvertisement(int id)
     {
-        
+        try
+        {
+            if (id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+
+            var advertisement = await _db.GetAsync(u => u.Id == id);
+
+            if (advertisement == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                return NotFound(_response);
+            }
+
+            _response.Result = _mapper.Map<AdvertisementDTO>(advertisement);
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages = new List<string>() { ex.ToString() };
+        }
+        return _response;
     }
     
 }
